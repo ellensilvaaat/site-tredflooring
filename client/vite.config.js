71700858cbb6f5d59802243c49ab716eb77cd443
ellensilvaaat <ellensilvaeng@gmail.com>
2025-11-30 21:48:16 +1100

    ultima tentativa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // 🔥 ISSO corrige os caminhos dos CSS/JS no deploy
   plugins: [react()],
-})
+});
