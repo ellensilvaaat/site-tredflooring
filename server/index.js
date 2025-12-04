e7ff1f0dcb11557c259aaad0a77e8ef7e06a70ea
ellensilvaaat <ellensilvaeng@gmail.com>
2025-12-04 13:42:43 +1100

    ajustes backend

--- server/index.js
+++ server/index.js
@@ -13,10 +13,11 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? 'https://tredflooring.com.au'
-    : '*', // ou 'http://localhost:3000' durante dev
+    ? ['https://tredflooring.com.au', 'https://www.tredflooring.com.au']
+    : '*',
   credentials: true
 }));
+
 
 app.use(express.json());
 
