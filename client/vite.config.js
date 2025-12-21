e80a29291a504ae3bec3a5f898f4d8bcc00738b1
ellensilvaaat <ellensilvaeng@gmail.com>
2025-12-21 16:06:12 +1100

    b

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // 🔥 ISSO corrige os caminhos dos CSS/JS no deploy
   plugins: [react()],
 });
